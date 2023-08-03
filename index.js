c899c8ca09f713d71ab618c7b04aa64916897e10
soomin <wnghkdsns@naver.com>
2023-08-03 11:56:50 +0900

    3 server 설정

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-var http = require('http');
-var hostname = '127.0.0.1'; 
-var port = '8080';
+const express = require('express');
+const cors = require('cors');
+const app = express(); 
+const port = '8080';
 
-const server=http.createServer(function(req,res){
-    const path=req.url;
-    const method=req.method;
+app.use(express.json());
+app.use(cors());  //브라우저의 cors 이슈를 막기위해 사용하는 코드
 
-    if(path==='/products'){
-        if(method==="GET"){  //가져오는거
-            res.writeHead(200,{"Content-Type":"application/json"});
-            const products=JSON.stringify([
-                {
-                    name:"시계",
-                    price:89000
-                }
-            ]);
-            res.end(products);
-        }else if(method==="POST"){  //입력하는거
-            res.end("생성되었습니다")
-        }
-    }
-    res.end("bye")
+app.get('/products',(req,res)=>{
+    const query=req.query;
+    console.log('queryString',query)
+    res.send({
+        products:[
+            {id:"0", name : "시계", price:79000, seller : "haru", imageUrl:"goodsShop/img/products/product01.jpg"},
+            {id:"1", name : "사이드 테이블", price:91000, seller : "gooreum", imageUrl:"goodsShop/img/products/product02.jpg"},
+            {id:"2", name : "침구", price:55000, seller : "nacho", imageUrl:"goodsShop/img/products/product03.jpg"}
+        ]
+    })
 })
 
-server.listen(port,hostname);
-console.log('server on')
+
+
+app.post('/products',(req,res)=>{
+    const body=req.body;
+    res.send({
+        body,
+    })
+})
+
+app.get("/products/:id", (req, res)=>{
+    const params=req.params;
+    const {id} = params;
+    res.send(`id는 ${id} 입니다`);
+})
+
+app.listen(port, ()=>{
+    console.log('server on')
+});
+
